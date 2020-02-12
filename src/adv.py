c80cf1f6c0ffd9e92e36bf496e7a3715db9651d3
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-02-12 16:33:42 +0100

    created simple REPL

--- src/adv.py
+++ src/adv.py
@@ -1,51 +1,85 @@
 from room import Room
+from player import Player
+
+
+class Game:
+    def __init__(self, room, player):
+        self.room = room
+        self.player = player
+
+    def __str__(self):
+        output = ''
+
+        return 'output'
+
+
+selection = ''
+direction = ('n', 's', 'e', 'w')
+while selection != 'q':
+    selection = input(
+        '\nEnter [n] for North \nEnter [s] for South \nEnter [e] for East \nEnter [w] for West \n')
+    try:
+        if selection == 'n':
+            print(f"You went North")
+        elif selection == 's':
+            print(f"You went South")
+        elif selection == 'e':
+            print(f"You went East")
+        elif selection == 'w':
+            print(f"You went West")
+        else:
+            print("")
+    except ValueError:
+        print('Please enter a valid choice:\n [n]\n[s]\n[e]\n[w]')
+
+print("Goodbye")
 
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+# room = {
+#     'outside':  Room("Outside Cave Entrance",
+#                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+# passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+# into the darkness. Ahead to the north, a light flickers in
+# the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+# to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
+#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+# chamber! Sadly, it has already been completely emptied by
+# earlier adventurers. The only exit is to the south."""),
+# }
 
 
-# Link rooms together
+# # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
 
-#
-# Main
-#
+# #
+# # Main
+# #
 
-# Make a new player object that is currently in the 'outside' room.
+# # Make a new player object that is currently in the 'outside' room.
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+# # Write a loop that:
+# #
+# # * Prints the current room name
+# # * Prints the current description (the textwrap module might be useful here).
+# # * Waits for user input and decides what to do.
+# #
+# # If the user enters a cardinal direction, attempt to move to the room there.
+# # Print an error message if the movement isn't allowed.
+# #
+# # If the user enters "q", quit the game.
